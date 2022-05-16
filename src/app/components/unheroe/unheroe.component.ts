3abf3759319e573ffeb8eeb1c3a867f7ff712167
Eduardo Marmolejo <70274376+marmolejox@users.noreply.github.com>
2022-05-15 22:49:41 -0500

    Componentes Commit

---
+++ src/app/components/unheroe/unheroe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unheroe',
+  templateUrl: './unheroe.component.html',
+  styleUrls: ['./unheroe.component.css']
+})
+export class UnheroeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
